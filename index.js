a24604ac535a6b0b4c3ca7cc373e1a1ab3651e99
Renan Kozuka Micali <renanmicali@hotmail.com>
2023-10-05 14:55:21 +1100

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+let num1 = 2
+let num2 = 31
+let multiply = num1 * num2 
 
-// Write your code here
+let random = parseInt(Math.random()*100)
+
+let num3 = 14
+let num4 = 10
+
+let mod = num3%num4
+
+let max = Math.max(10,5,2,3,20)
